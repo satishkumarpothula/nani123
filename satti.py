3fc546d252ee3e2e8d1320e08ad97c8399abedca
satishkumarpothula <45143296+satishkumarpothula@users.noreply.github.com>
2020-09-14 16:56:53 +0530

    this is first commit

--- satti.py
+++ satti.py
@@ -1,5 +1,5 @@
 Python 3.9.0a6 (tags/v3.9.0a6:bc1c8af, Apr 27 2020, 21:05:45) [MSC v.1924 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> print("hai")
+>>> print("hai this is satish")
 hai
 >>> 
